4f4dddbd14d8881c707aba7ce19a11735b8e6f1d
mdasfaq123 <mdasfaqahbabmayaz@gmail.com>
2025-12-16 15:53:35 +0600

    Add files via upload

---
+++ my_template.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define f0(i, n) for(ll i = 0; i < n; i++)
+#define f1(i, n) for(ll i = 1; i <= n; i++)
+void solve()
+{
+    
+}
+int32_t main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
