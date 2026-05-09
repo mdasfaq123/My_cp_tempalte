5c0a58d83ac6335af9a7b81cf7667836ddedffd6
Md. Asfaq Ahbab Mayaz <mdasfaqahbabmayaz@gmail.com>
2026-05-09 15:09:20 +0600

    Add files via upload

---
+++ CP_Template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+using ll = long long;
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (ll)(x).size()
+#define f0(i, n) for(ll i = 0; i < n; i++)
+#define f1(i, n) for(ll i = 1; i <= n; i++)
+#define testcase ll t; cin >> t; while(t--)
+#define testcase1 ll t; t = 1; while(t--)
+void solve()
+{
+    
+}
+int32_t main()
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    testcase1
+    {
+        solve();
+    }
+    return 0;
+}
